320232f6b4ad9a98fcb7904ae36022ab275320b4
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-10-03 16:07:27 +0900

    許可された標準ライブラリについてのヘッダー追加

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,14 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tkatsumata <tkatsumata@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 04:26:06 by marvin            #+#    #+#             */
-/*   Updated: 2025/10/03 04:26:38 by marvin           ###   ########.fr       */
+/*   Updated: 2025/10/03 15:51:13 by tkatsumata       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h> 
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <dirent.h>
+# include <term.h>
 
 #endif
