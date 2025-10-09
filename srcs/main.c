bf7ae669fe154c6c129bc5554014587dd2b9e10a
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-10-09 09:53:24 +0000

    ctrl-c capture, test for norminette failure

--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,36 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:13:40 by tkatsumata        #+#    #+#             */
-/*   Updated: 2025/10/09 03:11:32 by marvin           ###   ########.fr       */
+/*   Updated: 2025/10/09 09:52:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "libft.h"
+#include <signal.h>
+#define FT_PROMPT "> "
+
+void	sighandler(int signal)
+{
+	if (signal == SIGINT)
+	{
+		printf("SIGINT CAPTURED (ctrl-C)\n");
+		exit(0);
+	}
+}
 
 int	main(int argc, char **argv)
 {
-	char	*tmp;
+	char *input;
 
-	tmp = readline("> ");
 	(void)argc;
 	(void)argv;
-	printf("\n\ntmp -> %s\n", tmp);
+	input = NULL;
+	while (1)
+	{
+		input = readline(FT_PROMPT);
+		printf("\n\ntmp -> %s\n", input);
+		signal(SIGINT, sighandler);
+	}
 	return (0);
 }
