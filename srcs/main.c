e01ea2502217727a3ba16cd503e9599606587e28
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-10-17 05:48:22 +0000

    global variable g_sig_received added.

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,13 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:13:40 by tkatsumata        #+#    #+#             */
-/*   Updated: 2025/10/16 02:38:31 by marvin           ###   ########.fr       */
+/*   Updated: 2025/10/16 14:10:28 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+volatile sig_atomic_t	g_sig_received = 0;
 
 int	main(int argc, char **argv)
 {
@@ -18,6 +20,7 @@ int	main(int argc, char **argv)
 	t_loglevel	log_level;
 
 	log_level = LOG_QUIET;
+	log_level = LOG_DEBUG;
 	if (argc == 1)
 		exit_status = interactive_shell(argc, argv, log_level);
 	else if (argc >= 2)
