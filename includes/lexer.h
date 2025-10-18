a6f376912081f0676c20c68b9516b850c350c841
tez-dev <motolo.dev@gmail.com>
2025-10-18 11:27:45 +0900

    Supported for norm test

--- includes/lexer.h
+++ includes/lexer.h
@@ -13,9 +13,9 @@
 #ifndef LEXER_H
 # define LEXER_H
 
-typedef struct s_token t_token;
-typedef struct s_redirection t_redirection;
-typedef struct s_pipeline	t_pipeline;
+typedef struct s_token			t_token;
+typedef struct s_redirection	t_redirection;
+typedef struct s_pipeline		t_pipeline;
 
 // lexer status
 typedef enum e_state
@@ -23,7 +23,7 @@ typedef enum e_state
 	S_NORMAL,
 	S_DQUOTE,
 	S_SQUOTE
-} t_state;
+}								t_state;
 
 // Token kind
 typedef enum e_tokenkind
@@ -34,55 +34,55 @@ typedef enum e_tokenkind
 	TK_REDI_OUT,
 	TK_REDI_HEREDOC,
 	TK_REDI_APPEND
-}					t_tokenkind;
+}								t_tokenkind;
 
 typedef struct s_lexer
 {
-	char *buf;
-	int			idx;
-	t_state		state;
-	t_token		*head;
-	t_token		*tail;
-}				t_lexer;
+	char						*buf;
+	int							idx;
+	t_state						state;
+	t_token						*head;
+	t_token						*tail;
+}								t_lexer;
 
 //  Decomposed into token kind and value only
 typedef struct s_token
 {
-	t_tokenkind		tk;
-	char			*val;
-	struct s_token	*next;
-}					t_token;
+	t_tokenkind					tk;
+	char						*val;
+	struct s_token				*next;
+}								t_token;
 
 // Auxiliary structure of redirection
 typedef struct s_redirection
 {
-	t_tokenkind		tk;
-	char			*file;
-	t_redirection	*next;
-}					t_redirection;
+	t_tokenkind					tk;
+	char						*file;
+	t_redirection				*next;
+}								t_redirection;
 
 // Single command Structure
 typedef struct s_cmd
 {
-	int				ac;
-	char			**av;
-	char			**ep;
-	t_redirection	*red;
-}					t_cmd;
+	int							ac;
+	char						**av;
+	char						**ep;
+	t_redirection				*red;
+}								t_cmd;
 
 // The entire pipline
 typedef struct s_pipeline
 {
-	t_cmd			*cmd;
-	t_pipeline		*next;
-}					t_pipeline;
+	t_cmd						*cmd;
+	t_pipeline					*next;
+}								t_pipeline;
 
-int extract_symbol_token(t_lexer *lex);
-int extract_char_token(t_lexer *lex);
+int								extract_symbol_token(t_lexer *lex);
+int								extract_char_token(t_lexer *lex);
 
-t_token *upsert_token(t_lexer *lex,t_tokenkind tk,char *start, int len);
-t_token *tokenize(char *input);
-void free_tokens(t_token *head);
-
+t_token							*upsert_token(t_lexer *lex, t_tokenkind tk,
+									char *start, int len);
+t_token							*tokenize(char *input);
+void							free_tokens(t_token *head);
 
 #endif
--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,40 +6,41 @@
 /*   By: htsutsum <htsutsum@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 23:26:50 by htsutsum          #+#    #+#             */
-/*   Updated: 2025/10/18 11:08:28 by htsutsum         ###   ########.fr       */
+/*   Updated: 2025/10/18 11:25:32 by htsutsum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "./libft/includes/libft.h"
 #include "lexer.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-static void _append_token_to_list(t_lexer *lex, t_token *new_token)
+static void	_append_token_to_list(t_lexer *lex, t_token *new_token)
 {
 	if (!lex->head)
-    {
-        lex->head = new_token;
-        lex->tail = new_token;
-    }
-    else
-    {
-        lex->tail->next = new_token;
-        lex->tail = new_token;
-    }
+	{
+		lex->head = new_token;
+		lex->tail = new_token;
+	}
+	else
+	{
+		lex->tail->next = new_token;
+		lex->tail = new_token;
+	}
 }
 
-t_token *upsert_token(t_lexer *lex,t_tokenkind tk,char *start, int len)
+t_token	*upsert_token(t_lexer *lex, t_tokenkind tk, char *start, int len)
 {
-	char *val;
-	t_token *new_token;
-	val = malloc((len + 1 )* sizeof(char));
-    if (!val)
+	char	*val;
+	t_token	*new_token;
+
+	val = malloc((len + 1) * sizeof(char));
+	if (!val)
 		return (NULL);
 	val[len] = '\0';
-    ft_strlcpy(val, start, len + 1);
+	ft_strlcpy(val, start, len + 1);
 	new_token = malloc(sizeof(t_token));
-	if(!new_token)
+	if (!new_token)
 	{
 		free(val);
 		return (NULL);
@@ -47,42 +48,42 @@ t_token *upsert_token(t_lexer *lex,t_tokenkind tk,char *start, int len)
 	new_token->tk = tk;
 	new_token->val = val;
 	new_token->next = NULL;
-	 _append_token_to_list(lex, new_token);
+	_append_token_to_list(lex, new_token);
 	return (new_token);
 }
 
-t_token *tokenize(char *input)
+t_token	*tokenize(char *input)
 {
-	t_lexer lex;
+	t_lexer	lex;
 
-	ft_bzero(&lex,sizeof(t_lexer));
+	ft_bzero(&lex, sizeof(t_lexer));
 	lex.buf = input;
-	while(lex.buf[lex.idx]!='\0')
+	while (lex.buf[lex.idx] != '\0')
 	{
-		if(ft_isspace(lex.buf[lex.idx]))
+		if (ft_isspace(lex.buf[lex.idx]))
 		{
 			lex.idx++;
-			continue;
+			continue ;
 		}
-		if(extract_symbol_token(&lex) == 0)
-			continue;
-		if(extract_char_token(&lex) == 0)
-			continue;
+		if (extract_symbol_token(&lex) == 0)
+			continue ;
+		if (extract_char_token(&lex) == 0)
+			continue ;
 		return (NULL);
 	}
 	return (lex.head);
 }
 
-void free_token(t_token *head)
+void	free_token(t_token *head)
 {
-    t_token *tmp;
+	t_token	*tmp;
 
-    while (head != NULL) {
-        tmp = head;
-        head = head->next;
-        if (tmp->val)
-            free(tmp->val);
-        free(tmp);
-    }
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		if (tmp->val)
+			free(tmp->val);
+		free(tmp);
+	}
 }
-
