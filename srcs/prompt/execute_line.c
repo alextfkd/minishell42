a6ff23aef58be03e79cd181d6cfbdd1710334ead
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-10-20 06:20:51 +0000

    Pipe testに渡す時のInteractive_shell周りを修正しました。

--- srcs/prompt/execute_line.c
+++ srcs/prompt/execute_line.c
@@ -12,27 +12,49 @@
 
 #include "minishell.h"
 
-static void	exec_line_1(t_ms_buf *ms_buf, t_loglevel log_level, int	*status);
-static void	exec_line_2(t_ms_buf *ms_buf, t_loglevel log_level, int	*status);
-static void	exec_line_3(t_ms_buf *ms_buf, t_loglevel log_level, int	*status);
-static void	exec_line_4(t_ms_buf *ms_buf, t_loglevel log_level, int	*status);
+static void	exec_line_1(
+				t_ms_buf *ms_buf,
+				char **envp,
+				t_loglevel	log_level,
+				int	*status
+				);
+static void	exec_line_2(
+				t_ms_buf *ms_buf,
+				char **envp,
+				t_loglevel	log_level,
+				int	*status
+				);
+static void	exec_line_3(
+				t_ms_buf *ms_buf,
+				char **envp,
+				t_loglevel	log_level,
+				int	*status
+				);
+static void	exec_line_4(
+				t_ms_buf *ms_buf,
+				char **envp,
+				t_loglevel	log_level,
+				int	*status
+				);
 
-void	exec_line(t_ms_buf *ms_buf, t_loglevel log_level, int *status)
+void	exec_line(
+	t_ms_buf *ms_buf, char **envp, t_loglevel log_level, int *status
+)
 {
 	if (ms_buf->tmp_buf != NULL && ms_buf->rl_buf == NULL)
-		exec_line_1(ms_buf, log_level, status);
+		exec_line_1(ms_buf, envp, log_level, status);
 	else if (ms_buf->tmp_buf != NULL && ms_buf->rl_buf != NULL)
-		exec_line_2(ms_buf, log_level, status);
+		exec_line_2(ms_buf, envp, log_level, status);
 	else if (ms_buf->tmp_buf == NULL && ms_buf->rl_buf == NULL)
-		exec_line_3(ms_buf, log_level, status);
+		exec_line_3(ms_buf, envp, log_level, status);
 	else if (ms_buf->tmp_buf == NULL && ms_buf->rl_buf != NULL)
-		exec_line_4(ms_buf, log_level, status);
+		exec_line_4(ms_buf, envp, log_level, status);
 	else
 		*status = 1;
 }
 
 static void	exec_line_1(
-	t_ms_buf *ms_buf, t_loglevel log_level, int	*status
+	t_ms_buf *ms_buf, char **envp, t_loglevel log_level, int	*status
 )
 {
 	log_debug("1. tmp_buf != NULL && rl_buf == NULL", log_level);
@@ -43,11 +65,13 @@ static void	exec_line_1(
 		return ;
 	}
 	free_tmp_buf(ms_buf);
-	*status = execute_cmd(ms_buf->sh_buf, log_level);
+	*status = execute_cmd(ms_buf->sh_buf, envp, log_level);
 	free_shell_buf(ms_buf);
 }
 
-static void	exec_line_2(t_ms_buf *ms_buf, t_loglevel log_level, int	*status)
+static void	exec_line_2(
+	t_ms_buf *ms_buf, char **envp, t_loglevel log_level, int	*status
+)
 {
 	log_debug("2. tmp_buf != NULL && rl_buf != NULL", log_level);
 	ms_buf->sh_buf = ft_strjoin(ms_buf->tmp_buf, ms_buf->rl_buf);
@@ -71,19 +95,24 @@ ms_buf->sh_buf[ft_strlen(ms_buf->sh_buf) - 1] == '\\')
 		}
 		return (free_shell_buf(ms_buf));
 	}
-	*status = execute_cmd(ms_buf->sh_buf, log_level);
+	*status = execute_cmd(ms_buf->sh_buf, envp, log_level);
 	free_shell_buf(ms_buf);
 }
 
-static void	exec_line_3(t_ms_buf *ms_buf, t_loglevel log_level, int	*status)
+static void	exec_line_3(
+	t_ms_buf *ms_buf, char **envp, t_loglevel log_level, int	*status
+)
 {
 	log_debug("3. tmp_buf == NULL && rl_buf == NULL", log_level);
 	(void)ms_buf;
+	(void)envp;
 	*status = 0;
 	exit_with_sigeof();
 }
 
-static void	exec_line_4(t_ms_buf *ms_buf, t_loglevel log_level, int	*status)
+static void	exec_line_4(
+	t_ms_buf *ms_buf, char **envp, t_loglevel log_level, int	*status
+)
 {
 	log_debug("4. tmp_buf == NULL && rl_buf != NULL", log_level);
 	if (ft_strlen(ms_buf->rl_buf) != 0 && \
@@ -101,7 +130,8 @@ ms_buf->rl_buf[ft_strlen(ms_buf->rl_buf) - 1] == '\\')
 		return ;
 	}
 	log_debug("4-2. Executing rl_input", log_level);
-	*status = execute_cmd(ms_buf->rl_buf, log_level);
+	log_debug(ms_buf->rl_buf, log_level);
 	add_history(ms_buf->rl_buf);
+	*status = execute_cmd(ms_buf->rl_buf, envp, log_level);
 	free_readline_buf(ms_buf);
 }
