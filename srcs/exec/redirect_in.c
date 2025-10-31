8b47f6f65d57ad190d0881a1a7e9078c63a10611
tez-dev <motolo.dev@gmail.com>
2025-11-01 00:39:31 +0900

    Modified redirect_io.c rediect_in.c redirect_out.c for norm

---
+++ srcs/exec/redirect_in.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect_in.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htsutsum <htsutsum@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 21:41:31 by htsutsum          #+#    #+#             */
+/*   Updated: 2025/11/01 00:35:52 by htsutsum         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	_setup_in(t_red *red)
+{
+	int	fd;
+
+	fd = open(red->file, O_RDONLY);
+	if (fd == -1)
+	{
+		perror(red->file);
+		return (1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+
+static int	_setup_heredoc(t_red *red)
+{
+	(void)red;
+	// int fd;
+	// fd = open(red->file, O_RDONLY);
+	// if (fd == -1)
+	// {
+	// 	perror(red->file);
+	// 	return (1);
+	// }
+	// dup2(fd, STDIN_FILENO);
+	// close(fd);
+	ft_putendl_fd("heredoc has not suppotred yet", 1);
+	return (0);
+}
+
+int	do_redirect_in(t_red *red)
+{
+	if (red->tk == TK_RED_IN)
+		return (_setup_in(red));
+	else if (red->tk == TK_RED_HEREDOC)
+		return (_setup_heredoc(red));
+	return (0);
+}
