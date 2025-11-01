742644f4b1a22e32c5f38e8902049e4576485940
tez-dev <motolo.dev@gmail.com>
2025-11-01 16:14:49 +0900

    Modified prompt/noninteractive_shell.c for norm error

--- srcs/prompt/noninteractive_shell.c
+++ srcs/prompt/noninteractive_shell.c
@@ -6,7 +6,7 @@
 /*   By: htsutsum <htsutsum@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 01:59:38 by marvin            #+#    #+#             */
-/*   Updated: 2025/10/31 22:12:20 by htsutsum         ###   ########.fr       */
+/*   Updated: 2025/11/01 16:13:37 by htsutsum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,7 @@
 
 int	non_interactive_shell(t_shell *shell, char **envp)
 {
-	t_app	app;
-
 	log_debug("MINISHELL NON-INTERACTIVE MODE", shell->loglevel);
-	setup_app(&app, envp);
-	execute_cmd(shell->argv[1], &app, shell->loglevel);
-	clear_app(&app);
+	execute_cmd(shell->argv[1], envp, shell->loglevel);
 	return (0);
 }
