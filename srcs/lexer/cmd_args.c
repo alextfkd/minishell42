c23d3c924c22c3b88db6bd7b75961ea4b07603fe
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-11-11 04:58:24 +0000

    ロジックの整理

--- srcs/lexer/cmd_args.c
+++ srcs/lexer/cmd_args.c
@@ -12,13 +12,15 @@
 
 #include "minishell.h"
 
-static int	_argv_loop(t_cmd *cmd, t_token **current_ptr, int *i_ptr,
-				t_token *end);
-static int	_set_argv(t_cmd *cmd, t_token *start, t_token *end);
 static int	_count_argc(t_token *start, t_token *end);
+static int	_is_valid_redirection_token(t_token *token);
+static char	**_aquire_argv(t_token *start, int argc);
+static void	_free_argv(char **argv);
 
-/* Set array of *char to t_cmd->argv. The redirection operator token
-and the following redirection target are excluded.
+/* Set array of *char to t_cmd->argv. 
+The *start token is included in argv.
+The redirection operator token and 
+the following redirection target are excluded.
 The length of array is n + 1, terminating with NULL.
 */
 int	set_cmd_argv(t_cmd *cmd, t_token *start, t_token *end)
@@ -28,87 +30,101 @@ int	set_cmd_argv(t_cmd *cmd, t_token *start, t_token *end)
 	if (!cmd)
 		log_debug("cmd is null in set_cmd_argv!", LOG_DEBUG);
 	argc = _count_argc(start, end);
-	log_debug("current argc", LOG_DEBUG);
-	printf("argc -> %d\n", argc);
 	if (argc < 0)
 	{
-		ft_putendl_fd(ERR_SYNTAX_TOKEN, 2);
-		return (1);
+		return (ft_putendl_fd(ERR_SYNTAX_TOKEN, 2), 1);
 	}
-	cmd->argv = (char **)ft_calloc(argc + 1, sizeof(char *));
+	cmd->argv = _aquire_argv(start, argc);
 	if (!cmd->argv)
 	{
 		perror("minishell: t_cmd->argv memory allocated error");
 		return (1);
 	}
-	if (_set_argv(cmd, start, end))
-		return (1);
 	cmd->argc = argc;
 	return (0);
 }
 
-static int	_argv_loop(t_cmd *cmd, t_token **current_ptr, int *i_ptr,
-		t_token *end)
-{
-	t_token	*current;
-
-	current = *current_ptr;
-	while (current != end)
-	{
-		if (is_red(current->tk))
-		{
-			current = current->next->next;
-		}
-		else if (current->tk == TK_CHAR)
-		{
-			cmd->argv[*i_ptr] = ft_strdup(current->data);
-			if (!cmd->argv[*i_ptr])
-				return (1);
-			(*i_ptr)++;
-			current = current->next;
-		}
-	}
-	*current_ptr = current;
-	return (0);
-}
-
-// set values of argv
-static int	_set_argv(t_cmd *cmd, t_token *start, t_token *end)
-{
-	t_token	*current;
-	int		i;
-
-	i = 0;
-	current = start;
-	if (_argv_loop(cmd, &current, &i, end))
-		return (1);
-	cmd->argv[i] = NULL;
-	return (0);
-}
-
-/* count TK_CHAR and increment argc */
+/* Count the number of following TK_CHAR tokens as argc.
+The *start token is included in argc.
+The redirection token and the TK_CHAR token follows the redirection token
+will not be included in argc.
+*/
 static int	_count_argc(t_token *start, t_token *end)
 {
-	t_token	*current;
+	t_token	*p_token;
 	int		argc;
 
 	argc = 0;
-	current = start;
-	while (current != end)
+	p_token = start;
+	while (p_token != end)
 	{
-		if (is_red(current->tk))
+		if (p_token->tk == TK_CHAR)
 		{
-			if (current->next == NULL || current->next->tk != TK_CHAR)
-				return (-1);
-			current = current->next->next;
-		}
-		else if (current->tk == TK_CHAR)
-		{
+			p_token = p_token->next;
 			argc++;
-			current = current->next;
+		}
+		else if (_is_valid_redirection_token(p_token))
+		{
+			p_token = p_token->next->next;
 		}
 		else
 			return (-1);
 	}
 	return (argc);
+}
+
+static char	**_aquire_argv(t_token *start, int argc)
+{
+	char	**argv;
+	t_token	*p_token;
+	int		i;
+
+	argv = (char **)ft_calloc(argc + 1, sizeof(char *));
+	if (!argv)
+		return (NULL);
+	i = 0;
+	p_token = start;
+	while (i < argc)
+	{
+		if (p_token->tk == TK_CHAR)
+		{
+			argv[i] = ft_strdup(p_token->data);
+			if (argv[i] == NULL)
+				return (_free_argv(argv), NULL);
+			i++;
+			p_token = p_token->next;
+		}
+		else if (_is_valid_redirection_token(p_token))
+		{
+			p_token = p_token->next->next;
+		}
+	}
+	return (argv);
+}
+
+static void	_free_argv(char **argv)
+{
+	int	i;
+
+	if (argv == NULL)
+		return ;
+	while (argv[i] != NULL)
+	{
+		free(argv[i++]);
+	}
+	return ;
+}
+
+static int	_is_valid_redirection_token(t_token *token)
+{
+	if (token == NULL)
+		return (0);
+	if (is_red(token->tk) != 0)
+		return (0);
+	if (token->next == NULL)
+		return (0);
+	if (token->next->tk == TK_CHAR)
+		return (1);
+	else
+		return (0);
 }
