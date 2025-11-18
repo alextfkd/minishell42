ee74168969011e38c0ceec7cecd0706ec31ab011
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-11-18 03:04:34 +0000

    コメント追加、Perrorのメッセージ追加、サブモジュールのアロケーション失敗を確認するロジックの整理。

--- srcs/shell/ft_shell.c
+++ srcs/shell/ft_shell.c
@@ -6,14 +6,24 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 15:09:57 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/11/18 02:40:07 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/11/18 03:02:59 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#define SHELL_ALLOC_FAIL_MSG "Failed to allocate t_shell object."
+#define BUF_APP_ALLOC_FAIL_MSG "Failed to allocate t_ms_buf or t_app object."
 
 static t_ms_buf	*_create_input_buffer(void);
 
+/**
+ * @brief Create a t shell object. Returns NULL if memory allocation failed.
+ * 
+ * @param argc
+ * @param argv
+ * @param envp
+ * @return t_shell* 
+ */
 t_shell	*create_t_shell(int argc, char **argv, char **envp)
 {
 	t_shell	*shell;
@@ -21,28 +31,34 @@ t_shell	*create_t_shell(int argc, char **argv, char **envp)
 	shell = (t_shell *)ft_calloc(sizeof(t_shell), 1);
 	if (shell == NULL)
 	{
-		perror("Failed to allocate t_shell object.");
+		perror(SHELL_ALLOC_FAIL_MSG);
 		return (NULL);
-	}
-	shell->ms_buf = _create_input_buffer();
-	if (shell->ms_buf == NULL)
-	{
-		return (free(shell), NULL);
 	}
 	shell->argc = argc;
 	shell->argv = argv;
 	shell->loglevel = LOGLEVEL;
 	shell->status = 0;
+	shell->ms_buf = _create_input_buffer();
 	shell->app = setup_app(envp, &(shell->status));
-	if (shell->app == NULL)
-		return (NULL);
+	if (shell->ms_buf == NULL || shell->app == NULL)
+	{
+		perror(BUF_APP_ALLOC_FAIL_MSG);
+		return (free_shell(shell), NULL);
+	}
 	return (shell);
 }
 
-// Create t_ms_buf
-// rl_buf: readline() buffer.
-// tmp_buf: tempolary buffer, used when the command is separated with \newline.
-// sh_buf: buffer passed to execute_shell().
+/**
+ * @brief Create t_ms_buf.
+ * 
+ * rl_buf: readline() buffer.
+ * 
+ * tmp_buf: tempolary buffer, used when the command is separated with \newline.
+ * 
+ * sh_buf: buffer passed to execute_shell().
+ * 
+ * @return t_ms_buf* (NULL if malloc failed.)
+ */
 static t_ms_buf	*_create_input_buffer(void)
 {
 	t_ms_buf	*ms_buf;
