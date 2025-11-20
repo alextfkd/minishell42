e819be4c98210c34714de45e977b3ab2682d4937
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-11-20 03:17:43 +0000

    non interactive 一応動く。

--- srcs/prompt/noninteractive_shell.c
+++ srcs/prompt/noninteractive_shell.c
@@ -6,7 +6,7 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 01:59:38 by marvin            #+#    #+#             */
-/*   Updated: 2025/11/18 15:29:52 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/11/20 03:01:32 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,28 @@
 int	non_interactive_shell(t_shell *shell)
 {
 	log_debug("MINISHELL NON-INTERACTIVE MODE", shell->loglevel);
+	int	i;
+	i = 1;
+	while (i < shell->argc)
+	{
+		shell->ms_buf->rl_buf = ft_strjoin(shell->argv[i++], " ");
+		if (shell->ms_buf->rl_buf == NULL)
+		{
+			free_ms_buf(shell->ms_buf);
+			return (1);
+		}
+		if (shell->ms_buf->tmp_buf == NULL)
+			shell->ms_buf->sh_buf = ft_strdup(shell->ms_buf->rl_buf);
+		else
+			shell->ms_buf->sh_buf = ft_strjoin(shell->ms_buf->tmp_buf, shell->ms_buf->rl_buf);
+		if (shell->ms_buf->sh_buf == NULL)
+		{
+			free_ms_buf(shell->ms_buf);
+			return (1);
+		}
+		if (i != shell->argc)
+			shell->ms_buf->tmp_buf = shell->ms_buf->sh_buf;
+	}
 	pipeline_executor(shell);
 	return (0);
 }
