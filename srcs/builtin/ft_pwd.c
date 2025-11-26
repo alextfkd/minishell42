920585e20950d65d4c2ce65bfaa5ef68b834f407
tez-dev <85144666+tez-dev@users.noreply.github.com>
2025-11-26 18:48:56 +0900

    modified ft_pwd.c

--- srcs/builtin/ft_pwd.c
+++ srcs/builtin/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: htsutsum <htsutsum@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 11:34:42 by htsutsum          #+#    #+#             */
-/*   Updated: 2025/11/26 18:03:59 by htsutsum         ###   ########.fr       */
+/*   Updated: 2025/11/26 18:07:38 by htsutsum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	ft_pwd(t_app *app, t_cmd *cmd)
 {
 	char	*path;
 
+	(void)app;
 	path = get_current_dir();
 	if (!path)
 		return (print_builtin_cmd_error(cmd, 0, strerror(errno), 0), 1);
