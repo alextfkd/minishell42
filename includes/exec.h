0729faee8f1396a31687518b46316af6dee0f1e1
tez-dev <85144666+tez-dev@users.noreply.github.com>
2025-11-27 10:46:13 +0900

    Fixed a bug in here_doc expansion. heredoc_expand.c

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: htsutsum <htsutsum@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 22:50:00 by htsutsum          #+#    #+#             */
-/*   Updated: 2025/11/27 02:39:20 by htsutsum         ###   ########.fr       */
+/*   Updated: 2025/11/27 10:27:53 by htsutsum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ int					handle_redirections(t_red *red, t_app *app);
 
 // heredoc
 int					handle_heredoc(t_red *red, t_app *app);
-char				*expand_heredoc_line(char *line, t_app *app);
+char				*expand_line(char *line, t_app *app);
 void				close_heredoc_unused_fds(int *pipe_fds, t_app *app);
 void				restore_heredoc_std_io(t_app *app, int *pipe_fds);
 void				print_heredoc_error(char *delimiter);
