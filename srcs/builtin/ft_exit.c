01b4053c6f0c544d322847ef00469aaf2e5c7485
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-11-28 10:09:47 +0000

    Exitの引数対応、２５６以上の時の歩数表現対応。Numeric errorの際はStatus ０でExitするのは仕様です。

--- srcs/builtin/ft_exit.c
+++ srcs/builtin/ft_exit.c
@@ -6,11 +6,14 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 07:29:47 by htsutsum          #+#    #+#             */
-/*   Updated: 2025/11/27 15:59:07 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/11/28 10:08:43 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	_is_overflow(const char *nptr);
+static int	_is_alphasign(const char *str);
 
 /**
  * @brief exit test func,we have to program it correctly.
@@ -21,10 +24,25 @@
  */
 int	ft_exit(t_app *app, t_cmd *cmd)
 {
-	(void)cmd;
-	ft_putendl_fd("exit", 1);
-	exit_process(0, app);
-	return (0);
+	int	status;
+
+	if (cmd->argc == 1)
+		exit_process(0, app);
+	else if (cmd->argc == 2)
+	{
+		if (!_is_overflow(cmd->argv[1]) && _is_alphasign(cmd->argv[1]))
+		{
+			status = ft_atoi(cmd->argv[1]);
+			if (status < 0)
+				status = 256 - ((status * -1) / 256);
+			else
+				status = status / 256;
+			exit_process(status, app);
+		}
+		ft_putendl_fd("minishell: exit: numeric argument required", 2);
+		exit_process(0, app);
+	}
+	return (ft_putendl_fd("minishell: exit: too many arguments", 2), 1);
 }
 
 void	exit_process(int status, t_app *app)
@@ -38,4 +56,48 @@ void	exit_process(int status, t_app *app)
 	app->shell->ms_buf = NULL;
 	free_shell(app->shell);
 	exit(status);
+}
+
+static int	_is_alphasign(const char *str)
+{
+	const char	*p;
+
+	if (str == NULL)
+		return (0);
+	p = str;
+	if (ft_isdigit(*p) == 1 || *p == '-' || *p == '+')
+		p++;
+	while (*p != '\0')
+	{
+		if (ft_isdigit(*p) == 0)
+			return (0);
+		p++;
+	}
+	return (1);
+}
+
+static int	_is_overflow(const char *nptr)
+{
+	long	res;
+	long	sign;
+
+	res = 0;
+	sign = 1;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign *= -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr) != 0)
+	{
+		if (sign == 1 && res > INT_MAX / 10)
+			return (1);
+		if (sign == 1 && 10 * res > INT_MAX - (*nptr - '0'))
+			return (1);
+		res = 10 * res + (*nptr++ - '0');
+	}
+	if (sign == -1 || res > INT_MAX)
+		return (1);
+	return (0);
 }
