81021253088f567becb4c357617292ddddf1424a
tkatsuma <tkatsuma@student.42.fr>
2025-12-02 12:19:23 +0900

    Double backslash 対応済

--- srcs/prompt/integrate_input_buffer.c
+++ srcs/prompt/integrate_input_buffer.c
@@ -6,7 +6,7 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 03:25:27 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/12/02 11:47:13 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/12/02 12:18:47 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,41 @@
 static char		*_integrate_buffers(t_ms_buf *ms_buf);
 static	int		_is_executable(t_ms_buf *ms_buf, int *status);
 static t_token	*get_token_tail(t_token *head);
+static void		_free_tmp_and_rl_buf(t_ms_buf *ms_buf);
 
 char	*integrate_input_buffer(t_shell *shell)
 {
 	t_ms_buf	*ms_buf;
+	int			status;
 
 	ms_buf = shell->ms_buf;
 	ms_buf->sh_buf = _integrate_buffers(shell->ms_buf);
+	status = 0;
 	if (ms_buf->sh_buf == NULL)
+		status = 1;
+	else if (ft_strnstr(ms_buf->sh_buf, "\\\\", ft_strlen(ms_buf->sh_buf)))
 	{
-		shell->status = 1;
-		free_ms_buf(&ms_buf);
-		return (NULL);
+		status = 1;
+		ft_putendl_fd(ERR_DOUBLE_BACKSLASH, 2);
+	}
+	if (status)
+	{
+		_free_tmp_and_rl_buf(ms_buf);
+		return (free_shell_buf(ms_buf), NULL);
 	}
 	if (_is_executable(ms_buf, &(shell->status)) == 0)
 	{
-		free_tmp_buf(ms_buf);
-		free_readline_buf(ms_buf);
+		_free_tmp_and_rl_buf(ms_buf);
 		ms_buf->tmp_buf = ft_strdup(ms_buf->sh_buf);
-		free_shell_buf(ms_buf);
-		return (NULL);
+		return (free_shell_buf(ms_buf), NULL);
 	}
+	return (_free_tmp_and_rl_buf(ms_buf), ms_buf->sh_buf);
+}
+
+static void	_free_tmp_and_rl_buf(t_ms_buf *ms_buf)
+{
 	free_tmp_buf(ms_buf);
 	free_readline_buf(ms_buf);
-	return (ms_buf->sh_buf);
 }
 
 static char	*_integrate_buffers(t_ms_buf *ms_buf)
